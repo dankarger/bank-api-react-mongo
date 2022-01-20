4ad2bcf20a892185ba7e0520d2b3f3e9418bfc2d
dankarger <karger.dan@gmail.com>
2022-01-20 19:46:48 +0200

    add Api directory in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+// import { useState } from 'react';
+import myApi from './api/Api';
 
 function App() {
-  return (
-    <div className="App">
-     hello
-    </div>
-  );
+    // const [user, setUser] = useState('');
+
+    console.log(process.env.NODE_ENV);
+
+    const getReq = async () => {
+        const { data } = await myApi.get('/users');
+        console.log(data);
+    };
+    return (
+        <div className='App'>
+            {' '}
+            Hello World!
+            <button onClick={getReq}>get</button>
+        </div>
+    );
 }
 
 export default App;
